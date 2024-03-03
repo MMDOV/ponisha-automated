ad3fb4fd19db0ede30ef029938a836f6a61b2a42
MMDOV <mohammadvgamer@gmail.com>
2024-03-03 14:26:51 +0330

    first commit nothing is done yet just placeholders

---
+++ main.py
@@ -0,0 +1,27 @@
+# TODO: login and go to project list
+def login():
+    return None
+
+
+# TODO: grabs first project
+def grab_first_project():
+    first_project = None
+    return first_project
+
+
+# TODO: check if two projects are the same or not
+def compare_projects(previous_project, new_project):
+    return True or False
+
+
+# TODO: main operation(combining all the functions)
+#       login
+#       grab first project an save it in a variable
+#       compare project to the previous one and act accordingly
+#       set new project as previous project
+#       refresh the page
+
+
+# TODO: UI
+if __name__ == "__main__":
+    pass

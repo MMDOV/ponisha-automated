9672eebc7e7b33111e3ca62e3b657639559beb8d
MMDOV <mohammadvgamer@gmail.com>
2024-03-03 15:26:10 +0330

    added some small stuff

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import winsound
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox import service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoSuchWindowException, \
+    ElementNotInteractableException, ElementClickInterceptedException
+from webdriver_manager.firefox import GeckoDriverManager
+
+
 # TODO: login and go to project list
 def login():
+    option = Options()
+    option.page_load_strategy = "none"
+    driver = Firefox(options=option, service=service.Service(GeckoDriverManager().install()))
     return None
 
 

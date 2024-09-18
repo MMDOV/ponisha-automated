2d6ce146e2bf0b5bc040581a38bf5a95e5859744
mamadv <mohammadvary@gmail.com>
2024-09-18 22:51:19 +0330

    fixed a bug

--- run_main.sh
+++ run_main.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $SCRIPTPATH/venv/bin/activate
-pip install -r requirements.txt
+pip install -r $SCRIPTPATH/requirements.txt
 python $SCRIPTPATH/main.py

ff2c163b7916e1ea123a81f7f2dc7f64c9c24f1a
mamadv <mohammadvary@gmail.com>
2024-09-19 14:32:37 +0330

    small change

--- run_main.sh
+++ run_main.sh
@@ -2,4 +2,5 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 source $SCRIPTPATH/venv/bin/activate
 pip install -r $SCRIPTPATH/requirements.txt
+clear
 python $SCRIPTPATH/main.py
